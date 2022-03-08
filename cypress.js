ce444ef8728bfb788a4944941c3d57636afe24e2
Huynh Duc Duy <duy.huynh@linecorp.com>
2022-03-08 15:01:39 +0700

    Create cypress.js

---
+++ cypress.js
@@ -0,0 +1,15 @@
+'use strict';
+
+// Fix eslint shareable config (https://github.com/eslint/eslint/issues/3458)
+require('@rushstack/eslint-patch/modern-module-resolution');
+
+module.exports = {
+  plugins: ['jest', 'testing-library'],
+  overrides: [
+    {
+      files: ['**/cypress/**/*'],
+      extends: [
+        'plugin:cypress/recommended',
+    },
+  ],
+};
