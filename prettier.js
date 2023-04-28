a7e1a959a7372581fd7f8af0d926736a0382033b
Huynh Duc Duy <duy.huynh@linecorp.com>
2023-04-28 11:22:15 +0700

    Update prettier.js

--- prettier.js
+++ prettier.js
@@ -4,6 +4,5 @@ require('@rushstack/eslint-patch/modern-module-resolution');
 module.exports = {
   extends: [
     'plugin:prettier/recommended', // Always the last: https://github.com/prettier/eslint-plugin-prettier#recommended-configuration, with this we don't need to add `prettier` in plugins array
-    'prettier/prettier', // Fix some errors: https://github.com/prettier/eslint-plugin-prettier#arrow-body-style-and-prefer-arrow-callback-issue
   ],
 }
